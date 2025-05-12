b7b9f874a72f93a71438655d883cd2a95371ef0e
Toan Tran <khanh.toan.tran@wemove.com>
2025-05-12 14:55:42 +0200

    Update lineage source

--- classes/detail/parser/DetailParserMetadataIdfISO.php
+++ classes/detail/parser/DetailParserMetadataIdfISO.php
@@ -733,8 +733,7 @@ class DetailParserMetadataIdfISO
         $xpathExpression = "./gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:processStep/gmd:LI_ProcessStep/gmd:description/*[self::gco:CharacterString or self::gmx:Anchor]";
         $metadata->processStepDescriptions = IdfHelper::getNodeValueList($node, $xpathExpression);
 
-        $xpathExpression = "./gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:source/gmd:LI_Source/gmd:description/*[self::gco:CharacterString or self::gmx:Anchor]";
-        $metadata->sourceDescriptions = IdfHelper::getNodeValueList($node, $xpathExpression);
+        $metadata->sourceDescriptions = self::getLineageSource($node, $lang);
 
         $xpathExpression = "./gmd:identificationInfo/*/gmd:supplementalInformation/*[self::gco:CharacterString or self::gmx:Anchor]";
         $metadata->supplementalInformation = IdfHelper::getNodeValue($node, $xpathExpression);
@@ -1047,6 +1046,30 @@ class DetailParserMetadataIdfISO
         self::addToArray($array, "denominators", $denominators);
         self::addToArray($array, "dpis", $dpis);
         self::addToArray($array, "meters", $meters);
+        return $array;
+    }
+
+    private static function getLineageSource(\SimpleXMLElement $node, string $lang): array
+    {
+        $array = [];
+
+        $xpathExpression = "./gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:source/gmd:LI_Source[./gmd:description/*[self::gco:CharacterString or self::gmx:Anchor]]";
+        $tmpNodes = IdfHelper::getNodeList($node, $xpathExpression);
+        foreach ($tmpNodes as $tmpNode) {
+            $description = IdfHelper::getNodeValue($tmpNode, "./gmd:description/*[self::gco:CharacterString or self::gmx:Anchor]");
+            $title = IdfHelper::getNodeValue($tmpNode, "./gmd:sourceCitation/gmd:CI_Citation/gmd:title/*[self::gco:CharacterString or self::gmx:Anchor]");;
+            $date = IdfHelper::getNodeValue($tmpNode, "./gmd:sourceCitation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:date/*[self::gco:Date or self::gco:DateTime]");
+            $dateType = IdfHelper::getNodeValue($tmpNode, "./gmd:sourceCitation/gmd:CI_Citation/gmd:date/gmd:CI_Date/gmd:dateType/gmd:CI_DateTypeCode");
+            $identifier = IdfHelper::getNodeValue($tmpNode, "./gmd:sourceCitation/gmd:CI_Citation/gmd:identifier/gmd:MD_Identifier/gmd:code/*[self::gco:CharacterString or self::gmx:Anchor]");
+            $array[] = array(
+                "title" => $title,
+                "description" => $description,
+                "date" => $date,
+                "dateType" => $dateType ? CodelistHelper::getCodelistEntryByLocalisation('502', $dateType, $lang) : null,
+                "url" => $identifier
+            );
+        }
+
         return $array;
     }
 
