f134599167ee02fffdebd73cba80722a0fcbcdc6
Toan Tran <khanh.toan.tran@wemove.com>
2026-04-13 10:11:38 +0200

    Fix Rss with "false" return (##8927)
    (cherry picked from commit 458a86b3c9903276932307f613bc00092645e696)

--- classes/indexing/rss/RssIndex.php
+++ classes/indexing/rss/RssIndex.php
@@ -50,67 +50,69 @@ class RssIndex
         try {
             if (($response = HttpHelper::getHttpContent($url)) !== false) {
                 $content = simplexml_load_string($response);
-                $itemNodes = $content->xpath("//item");
-                $dateNow = new DateTime();
-                foreach ($itemNodes as $itemNode) {
-                    $title = (string)$itemNode->title;
-                    $link = (string)$itemNode->link;
-                    $date = (string)$itemNode->pubDate;
+                if ($content) {
+                    $itemNodes = $content->xpath("//item");
+                    $dateNow = new DateTime();
+                    foreach ($itemNodes as $itemNode) {
+                        $title = (string)$itemNode->title;
+                        $link = (string)$itemNode->link;
+                        $date = (string)$itemNode->pubDate;
 
-                    if (!$date) {
-                        # Extract date from url
-                        if (preg_match_all('/\d{2}\d{2}\d{2}/',$link,$matches)) {
-                            if ($matches) {
-                                $date = DateTime::createFromFormat("!ymd", $matches[0][0]);
-                                if ($date) {
-                                    $date = $date->format('Y-m-d T');
+                        if (!$date) {
+                            # Extract date from url
+                            if (preg_match_all('/\d{2}\d{2}\d{2}/',$link,$matches)) {
+                                if ($matches) {
+                                    $date = DateTime::createFromFormat("!ymd", $matches[0][0]);
+                                    if ($date) {
+                                        $date = $date->format('Y-m-d T');
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    if ($date) {
-                        $tmpDate = date_create($date);
-                        if ($tmpDate) {
-                            $rssDate = date_format($tmpDate, "d.m.y");
-                            $rssTime = date_format($tmpDate, "H:i");
-                            $rssDateMs = (float)date_format($tmpDate, "Uv");
-                        }
-                    }
-
-                    if ($date) {
-                        if ($dateNow < new DateTime($date)) {
-                            break;
-                        }
-                    }
-                    $description = strip_tags((string)$itemNode->description);
-
-                    $item = array(
-                        "title" => $title,
-                        "url" => $link,
-                        "date" => $rssDate ?? $dateNow->format('d.m.Y'),
-                        "time" => isset($rssTime) && $rssTime != "00:00" ? $rssTime : null,
-                        "date_ms" => $rssDateMs ?? $dateNow->format('uv'),
-                        "summary" => $description,
-                        "provider" => $summary ?? $provider,
-                    );
-
-                    if ($date) {
-                        $array[] = $item;
-                    } else {
-                        $existItem = false;
-                        if (isset($existingRss['data'])) {
-                            foreach ($existingRss['data'] as $rssItem) {
-                                if ($rssItem['url'] === $link && $rssItem['title'] === $title) {
-                                    $existItem = $rssItem;
-                                    break;
-                                }
+                        if ($date) {
+                            $tmpDate = date_create($date);
+                            if ($tmpDate) {
+                                $rssDate = date_format($tmpDate, "d.m.y");
+                                $rssTime = date_format($tmpDate, "H:i");
+                                $rssDateMs = (float)date_format($tmpDate, "Uv");
                             }
                         }
-                        if ($existItem) {
-                            $array[] = $existItem;
-                        } else {
+
+                        if ($date) {
+                            if ($dateNow < new DateTime($date)) {
+                                break;
+                            }
+                        }
+                        $description = strip_tags((string)$itemNode->description);
+
+                        $item = array(
+                            "title" => $title,
+                            "url" => $link,
+                            "date" => $rssDate ?? $dateNow->format('d.m.Y'),
+                            "time" => isset($rssTime) && $rssTime != "00:00" ? $rssTime : null,
+                            "date_ms" => $rssDateMs ?? $dateNow->format('uv'),
+                            "summary" => $description,
+                            "provider" => $summary ?? $provider,
+                        );
+
+                        if ($date) {
                             $array[] = $item;
+                        } else {
+                            $existItem = false;
+                            if (isset($existingRss['data'])) {
+                                foreach ($existingRss['data'] as $rssItem) {
+                                    if ($rssItem['url'] === $link && $rssItem['title'] === $title) {
+                                        $existItem = $rssItem;
+                                        break;
+                                    }
+                                }
+                            }
+                            if ($existItem) {
+                                $array[] = $existItem;
+                            } else {
+                                $array[] = $item;
+                            }
                         }
                     }
                 }
